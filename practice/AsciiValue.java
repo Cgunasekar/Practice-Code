588c7f3135f45b75d7927d86e5f3b429166765d3
Cgunasekar <97549991+Cgunasekar@users.noreply.github.com>
2022-04-01 12:06:23 +0530

    Add files via upload

---
+++ practice/AsciiValue.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class AsciiValue {
+
+	public static void main(String[] args) {
+		int arr[] = {67,72,73,84,82,65};
+		for(int i=0; i<arr.length;i++)
+		{
+			System.out.print((char)arr[i]+"");
+	
+		}
+
+	}
+
+}
